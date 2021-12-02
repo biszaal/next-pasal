c7eed556a3bc96bbbe47373b499be652ed648e5a
biszaal <biszaal@yahoo.com>
2021-12-02 09:46:24 +0545

    add to cart fixed

--- pages/api/products/[id]/index.js
+++ pages/api/products/[id]/index.js
@@ -1,10 +1,8 @@
 import nc from "next-connect";
-import { isAdmin, isAuth } from "../../../../utils/auth";
 import Product from "../../../../models/Product";
 import db from "../../../../utils/db";
 
 const handler = nc();
-handler.use(isAuth, isAdmin);
 
 handler.get(async (req, res) => {
   await db.connect();
